073a0278e67c2a8fd7987f38be1a3c87aea5f548
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2015-01-12 14:20:04 +0100

    Added missing bytes conversion

--- pylibmodbus/modbus_rtu.py
+++ pylibmodbus/modbus_rtu.py
@@ -9,4 +9,4 @@ class ModbusRtu(ModbusCore):
     def __init__(self, device="/dev/ttyS0", baud=9600,
             parity='N', data_bit=1, stop_bit=0):
         self.ctx = C.modbus_new_rtu(device.encode(), baud,
-                parity, data_bit, stop_bit)
+                parity.encode(), data_bit, stop_bit)
