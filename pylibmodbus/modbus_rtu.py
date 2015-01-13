80da9181d04b803eb1d7e19213ee9ebc37f97cb7
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2015-01-13 15:39:10 +0100

    Added some configuration constants

--- pylibmodbus/modbus_rtu.py
+++ pylibmodbus/modbus_rtu.py
@@ -4,8 +4,15 @@
 
 from .modbus_core import C, ModbusCore
 
-
 class ModbusRtu(ModbusCore):
+
+    RTU_RS232 = 0x00
+    RTU_RS232 = 0x01
+
+    RTU_RTS_NONE = 0x00
+    RTU_RTS_UP = 0x01
+    RTU_RTS_DOWN = 0x02
+
     def __init__(self, device="/dev/ttyS0", baud=9600,
             parity='N', data_bit=1, stop_bit=0):
         self.ctx = C.modbus_new_rtu(device.encode(), baud,
