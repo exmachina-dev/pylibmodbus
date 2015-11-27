7307f42cf954af7a952ef9a3d40e39fc97a14193
Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>
2015-11-27 14:08:47 +0900

    modbus_rtu: fix arguments to initialize libmodbus.
    Signed-off-by: Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>

--- pylibmodbus/modbus_rtu.py
+++ pylibmodbus/modbus_rtu.py
@@ -8,4 +8,9 @@ from .modbus_core import C, ModbusCore
 
 class ModbusRtu(ModbusCore):
     def __init__(self, device="/dev/ttyS0", baud=19200, parity="N", data_bit=8, stop_bit=1):
-        self.ctx = C.modbus_new_rtu(device, baud, parity, data_bit, stop_bit)
+        if type(device) == str:
+            device = device.encode("latin1")
+        self.ctx = C.modbus_new_rtu(device, baud, ord(parity[0]), data_bit, stop_bit)
+
+        if self.ctx == 0:
+            raise
