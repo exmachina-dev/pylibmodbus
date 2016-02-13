42b2d1619a6f1ffbfafcde9807ed008af50e044d
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2016-02-13 13:07:17 +0100

    Include ModbusException as base Exception

--- pylibmodbus/__init__.py
+++ pylibmodbus/__init__.py
@@ -4,5 +4,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 from .modbus_core import get_float, set_float, cast_to_int16, cast_to_int32
+from .modbus_core import ModbusException
 from .modbus_tcp import ModbusTcp
 from .modbus_rtu import ModbusRtu
